b59d77d48e1c40ca36e074c48715f68d393f8a88
Mauricio Juarez <mjuarez@multi-prets.ca>
2018-09-18 20:24:57 -0400

    Test commit Mauri

--- myclinic/settings.py
+++ myclinic/settings.py
@@ -6,6 +6,7 @@ PROJECT_PACKAGE = Path(__file__).resolve().parent.parent
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Mauri test
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
