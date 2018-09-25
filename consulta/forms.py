79b7829b655fc27e92bbff1800bb5d35364dc7ba
William Juarez <will@Williams-MacBook-Pro.local>
2018-09-25 05:48:50 -0600

    Seguimiento de ingreso de expediente, validacion de form

--- consulta/forms.py
+++ consulta/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 
 class ExpedienteForm(forms.Form):
-    id = forms.IntegerField,
-    id_horario = forms.IntegerField,
-    id_paciente = forms.IntegerField,
-    fecha = forms.DateField,
-    problema = forms.CharField,
-    diagnostico = forms.CharField,
-    tratamiento = forms.CharField,
-    recomendacion = forms.CharField,
-    talla = forms.DecimalField,
-    peso = forms.DecimalField
-    pres_art = forms.DecimalField
+    id_int = forms.IntegerField(),
+    id_horario = forms.IntegerField(),
+    id_paciente = forms.IntegerField(),
+    fecha = forms.DateField(),
+    problema = forms.CharField(),
+    diagnostico = forms.CharField(),
+    tratamiento = forms.CharField(),
+    recomendacion = forms.CharField(),
+    talla = forms.DecimalField(),
+    peso = forms.DecimalField(),
+    pres_art = forms.DecimalField()
--- consulta/urls.py
+++ consulta/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url('expediente/paciente', views.home, name="expediente_paciente"),
+    url('expediente/save/paciente', views.save, name="expediente_save"),
 ]
