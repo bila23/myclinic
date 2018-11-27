43d39baa33bbcb008b2d206ee5683915c69a15d7
William Juarez <will@Williams-MacBook-Pro.local>
2018-11-26 18:25:24 -0600

    Consulta de paciente (find by key)

--- paciente/urls.py
+++ paciente/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url('paciente/nuevo/', views.paciente_home, name="paciente_home"),
     url('paciente/find/key', views.find_paciente_by_key, name="paciente_find_by_key"),
     url('paciente/save', views.save_paciente, name="paciente_save"),
+    url('paciente/find/', views.find_pacientes, name="paciente_find"),
+    url('paciente/all', views.find_pacientes_all, name="paciente_find_all"),
+    url('paciente/get/single', views.get_paciente_by_key, name="get_paciente_by_key"),
 ]
--- paciente/views.py
+++ paciente/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from core.models import Paciente
@@ -48,3 +49,33 @@ def save_paciente(request):
     except Exception as inst:
         log.error(inst)
         return JsonResponse({'msg': 'Ha ocurrido un error al tratar de guardar el paciente', 'type': 'error'}, safe=False)
+
+@login_required
+def find_pacientes(request):
+        return render(request, 'paciente_consulta.html')
+
+
+@login_required
+def find_pacientes_all(request):
+        try:
+                pac_list = Paciente.objects.filter(id_medico = gf.findUser(request.user.username)).order_by('nombre')
+                data = serializers.serialize('json', pac_list)
+                return JsonResponse({'pac_list': data})
+        except Exception as inst:
+                log.error(inst)
+                return JsonResponse({'msg': 'Ha ocurrido un error inesperado al recuperar los pacientes', 'type': 'error'})
+
+@login_required
+def get_paciente_by_key(request):
+        try:
+                if (request.method == 'POST' and request.is_ajax()):
+                        id_pac = request.POST.get('id_pac')
+                        paciente = get_object_or_404(Paciente, pk=id_pac)
+                        array_result = serializers.serialize('json', [paciente], ensure_ascii = False)
+                        obj = array_result[1:-1]
+                        return JsonResponse({'paciente': obj, 'type': 'success'})
+                else:
+                        return JsonResponse({'msg': 'Ha ocurrido un error al tratar de acceder a la informaci&oacute;n del paciente', 'type': 'error'})
+        except Exception as inst:
+                log.error(inst)
+                return JsonResponse({'msg': 'Ha ocurrido un error al tratar de recuperar la informaci&oacute;n del paciente', 'type': 'error'})
